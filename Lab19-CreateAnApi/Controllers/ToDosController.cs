326aa948866a19235fb5e82458ad05a5925e7aa2
rebeccahong <rebeccayhong@gmail.com>
2018-11-04 00:46:03 -0700

    Add READ actions in ToDosController

--- Lab19-CreateAnApi/Controllers/ToDosController.cs
+++ Lab19-CreateAnApi/Controllers/ToDosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Lab19_CreateAnApi.Data;
+using Lab19_CreateAnApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -21,9 +22,18 @@ namespace Lab19_CreateAnApi.Controllers
 
         // GET api/ToDos
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<ToDo>> Get()
         {
-            return _context.ToDos.ToListAsync();
+            return _context.ToDos;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ToDo> Get(int? id)
+        {
+            var todo = _context.ToDos.FirstOrDefault(t => t.ID == id);
+            if (todo == null) return NotFound();
+
+            return Ok(todo);
         }
     }
 }
---
+++ Lab19-CreateAnApi/Migrations/20181104073822_changeAccessibilityOfNavProps.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Lab19_CreateAnApi.Migrations
+{
+    public partial class changeAccessibilityOfNavProps : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    DateTime = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    IsComplete = table.Column<bool>(nullable: false),
+                    DateTime = table.Column<string>(nullable: true),
+                    ToDoListID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ToDos_ToDoLists_ToDoListID",
+                        column: x => x.ToDoListID,
+                        principalTable: "ToDoLists",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_ToDoListID",
+                table: "ToDos",
+                column: "ToDoListID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+        }
+    }
+}
