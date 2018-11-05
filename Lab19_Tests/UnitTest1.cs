0764aa5c742ed67268e8328a7dbf1c9f11c85d41
rebeccahong <rebeccayhong@gmail.com>
2018-11-04 21:30:11 -0800

    Add Create ToDo test

--- Lab19_Tests/UnitTest1.cs
+++ Lab19_Tests/UnitTest1.cs
@@ -1,13 +1,28 @@
 using System;
 using Xunit;
+using Lab19_CreateAnApi.Data;
+using Microsoft.EntityFrameworkCore;
+using Lab19_CreateAnApi.Models;
+using System.Linq;
 
 namespace Lab19_Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CreateToDo()
         {
+            DbContextOptions<CreateAnAPIDbContext> options = new DbContextOptionsBuilder<CreateAnAPIDbContext>().UseInMemoryDatabase("ToDos").Options;
+
+            using (CreateAnAPIDbContext db = new CreateAnAPIDbContext(options))
+            {
+                ToDo td = new ToDo() { Name = "Walk the cat", IsComplete = true };
+                db.Add(td);
+                db.SaveChanges();
+
+                var foundToDo = db.ToDos.FirstOrDefault(todo => todo.ID == td.ID);
+                Assert.Equal(td.ID, foundToDo.ID);
+            }
 
         }
     }
